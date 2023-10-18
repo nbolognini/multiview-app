de90927d9d122e37e9acd61b3e700a7a3c362ae6
Arieu Sala <nbolognini@unimoron.edu.ar>
2023-10-18 16:55:35 -0300

    Latina Arreglado ok ok ok

--- src/pages/Latinatv.js
+++ src/pages/Latinatv.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
-
 import { Titulo } from "./estilosJS/Titulo";
 import { Contenedor } from "./estilosJS/Contenedor";
 import { Contenedor_Senales } from "./estilosJS/Contenedor_Senales";
 import { Senales } from "./estilosJS/Senales";
-
-
 
 const Latinatv = () =>{
     return <div>  
@@ -17,10 +14,9 @@ const Latinatv = () =>{
 						LatinaTV
 						<br></br>
 						HLS (nebula)<br></br><br></br>
-						<ReactPlayer controls muted playing width="264" height="198"   url='https://stream-gtlc.telecentro.net.ar/hls/radiolatinahls/main.m3u8' />					
+						<ReactPlayer controls muted playing width="264" height="198"   url='https://stream-gtlc.telecentro.net.ar/hls/latinatvhls/main.m3u8' />					
                     </Titulo>
 				</Senales>
-
 				<Senales>
 					<Titulo>
 						LatinaTV
@@ -29,25 +25,19 @@ const Latinatv = () =>{
 						<ReactPlayer controls muted playing width="264" height="198"   url='http://live-edge01.telecentro.net.ar/live/smil:rltn.smil/playlist.m3u8' />					
                     </Titulo>
 				</Senales>
-
 				<Senales>
 					<Titulo>
                         LatinaTV
 						<br></br>
 						(Twitch)
 					<iframe src="https://player.twitch.tv/?channel=radiolatina1011&parent=http://localhost" frameborder="0" allowfullscreen="true" scrolling="no" width="261" height="160"></iframe>
-
 					</Titulo>
 				</Senales>
-
 				<Senales>
-					<Titulo>
-                        
-						<br></br>
-										
+					<Titulo> 
+						<br></br>			
 					</Titulo>
 				</Senales>
-
 				<Senales>
 					<Titulo>
 						RadioLatina
@@ -56,7 +46,6 @@ const Latinatv = () =>{
 						<ReactPlayer controls muted={0} playing={0} width="264" height="198"   url='https://stream-gtlc.telecentro.net.ar/hls/latinatvhls/main.m3u8' />					
                     </Titulo>
 				</Senales>
-
                 <Senales>
 					<Titulo>
                         RadioLatina
@@ -65,7 +54,6 @@ const Latinatv = () =>{
 						<iframe src="http://streaming.latina101.com.ar:8080/RadioLatina" width="261" height="188"></iframe>
 					</Titulo>
 				</Senales>
-
 				<Senales>
 					<Titulo>
                         RadioLatina
@@ -74,10 +62,8 @@ const Latinatv = () =>{
 						<iframe src="http://streaming.latina101.com.ar:9000/Repetidoras" width="261" height="188"></iframe>
 					</Titulo>
 				</Senales>
-
 			</Contenedor_Senales>
 		</Contenedor>	
-
     </div>
    }   
    export default Latinatv;
